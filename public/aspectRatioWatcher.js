be75808252ca7d71c0cb802e0b559583bd87aba2
stepastepa <stepanisworking@gmail.com>
2025-04-20 21:32:04 +0300

    fix: improve aspectRatioWatcher

--- public/aspectRatioWatcher.js
+++ public/aspectRatioWatcher.js
@@ -7,19 +7,16 @@
 // window.addEventListener('resize', updateWideAspectClass);
 // window.addEventListener('orientationchange', updateWideAspectClass);
 
+
 const testQuery = '(min-aspect-ratio: 0.84/1)';
 const supportsAspectRatio = matchMedia(testQuery).media === testQuery;
 
-if (!supportsAspectRatio) {
-  function applyWideAspectRatioClass(entry) {
-    const rect = entry.contentRect;
-    const ratio = rect.width / rect.height;
-    document.documentElement.classList.toggle('wide-aspect-ratio', ratio >= 0.84);
-  }
-
+if (!supportsAspectRatio && 'ResizeObserver' in window) {
   const observer = new ResizeObserver(entries => {
-  for (let entry of entries) {
-      applyWideAspectRatioClass(entry);
+    for (let entry of entries) {
+      const rect = entry.contentRect;
+      const ratio = rect.width / rect.height;
+      document.documentElement.classList.toggle('wide-aspect-ratio', ratio >= 0.84);
     }
   });
 
