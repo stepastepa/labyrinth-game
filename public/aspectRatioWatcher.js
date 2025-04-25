5abe4420d69ae829bcefe039594ffb03e04716b8
stepastepa <stepanisworking@gmail.com>
2025-04-25 23:21:12 +0300

    fix: add new aspectRatioWatcher fallback

--- public/aspectRatioWatcher.js
+++ public/aspectRatioWatcher.js
@@ -10,14 +10,14 @@
 const query = '(min-aspect-ratio: 0.84/1)';
 const media = window.matchMedia(query);
 
-if (media.matches !== (window.innerWidth / window.innerHeight >= 0.84)) {
-  const observer = new ResizeObserver(entries => {
-    for (let entry of entries) {
-      const rect = entry.contentRect;
-      const ratio = rect.width / rect.height;
-      document.documentElement.classList.toggle('wide-aspect-ratio', ratio >= 0.84);
-    }
-  });
-
-  observer.observe(document.documentElement);
+function changeLayout() {
+  let root = document.querySelector("html");
+  if (media.matches !== (window.innerWidth / window.innerHeight >= 0.84)) {
+    root.classList.add('wide-aspect-ratio');
+  } else {
+    root.classList.remove('wide-aspect-ratio');
+  }
 }
+
+changeLayout(); // initial
+window.addEventListener('resize', changeLayout);
