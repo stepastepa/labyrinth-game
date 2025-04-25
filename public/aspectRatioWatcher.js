53329b66b2540194121935e9cc16f4ecb403d24e
stepastepa <stepanisworking@gmail.com>
2025-04-26 02:42:29 +0300

    fix: correct aspectRatioWatcher fallback

--- public/aspectRatioWatcher.js
+++ public/aspectRatioWatcher.js
@@ -1,15 +1,18 @@
-const query = '(min-aspect-ratio: 0.84/1)';
-const media = window.matchMedia(query);
+function isSupportAspRatio() {
+  return CSS.supports('min-aspect-ratio', '0.84');
+}
 
 function changeLayout() {
   let root = document.querySelector('html');
-  if (media.matches !== (window.innerWidth / window.innerHeight >= 0.84)) {
+  if (window.innerWidth / window.innerHeight >= 0.84) {
     root.classList.add('wide-aspect-ratio');
   } else {
     root.classList.remove('wide-aspect-ratio');
   }
 }
 
-changeLayout(); // initial
-window.addEventListener('resize', changeLayout);
-window.addEventListener('orientationchange', changeLayout);
+if (!isSupportAspRatio()) {
+  changeLayout(); // initial
+  window.addEventListener('resize', changeLayout);
+  window.addEventListener('orientationchange', changeLayout);
+}
